ff830b85bdb84f99ec117effc3847214471676fb
Pakaporn Tangnuntachai <pakaporn.tangnuntachai@gmail.com>
2019-12-06 20:38:46 +0100

    first script HET_micro3D

--- hole_expansion/submodel_Nid/micro_scale_3D/modelGeom.py
+++ hole_expansion/submodel_Nid/micro_scale_3D/modelGeom.py
@@ -17,17 +17,41 @@ import material
 import section
 import assembly
 import step
+import interaction
 import load
 import mesh
+import optimization
 import sketch
+import job
+import visualization
+import connectorBehavior
 import numpy as np
+import sys
 
 # ------------------------------------------------------------------------------------------------------------
 
 boxsize = 30
-isTopHalf = 0    # 1 for top-half mesh
-                 # 0 for full cubic mesh
-surfFile = 'recon_2D_149-1A1_30pnts_domAmp50.csv'
+surfFile = 'recon2D_Drill3_1_30pnts.csv'
+partition_ratio = 0.8
+
+# ------------------------------------------------------------------------------------------------------------
+
+# Import data points
+rough_data = np.genfromtxt(surfFile, delimiter=',', skip_header=1)
+samplingPt = int(rough_data.item((-1, 0)))
+
+if(samplingPt != boxsize):
+    print('wrong input file by boxsize')
+
+## Seperate points from each plane to list
+pt = []
+for i in range(boxsize + 1):
+    pt.append([[x-boxsize/2, y+boxsize/2] for [z,x,y] in rough_data if z == i])
+
+# find minimum height
+h = [y+boxsize/2 for [z,x,y] in rough_data]
+minH = np.amin(h)
+partitionH = partition_ratio * minH
 
 # ------------------------------------------------------------------------------------------------------------
 
@@ -35,127 +59,111 @@ surfFile = 'recon_2D_149-1A1_30pnts_domAmp50.csv'
 executeOnCaeStartup()
 myModel = mdb.models['Model-1']
 
-
-# Create sketch
-s1 = myModel.ConstrainedSketch(name='profile_1', sheetSize=0.05)
-s1.sketchOptions.setValues(decimalPlaces=6)
-s1.rectangle(point1=(-2.0*boxsize, -2.0*boxsize), point2=(2.0*boxsize, 2.0*boxsize)
-
-
-# Create part
-myPart = myModel.Part(name='elem', dimensionality=THREE_D, type=DEFORMABLE_BODY)
-
-
-# Create datum plane
-plane_1 = myPart.DatumPlaneByPrincipalPlane(principalPlane=XYPLANE, offset=0)
-
-
-# Create wire
-myPart.Wire(sketchPlane=plane_1, sketchPlaneSide=SIDE1, sketchUpEdge=YAXIS, sketch=s1)
-# profile_2 = myModel.myPart.Wire(sketchPlane=plane_2, sketchPlaneSide=SIDE1, sketchUpEdge=YAXIS, sketch=s2)
-
-
-'''
-Path
-
-mdb.models[name].parts[name].Wire
-
-
-Required arguments
-sketchPlane
-A Datum plane object or a planar Face object specifying the plane on which to sketch.
-sketchPlaneSide
-A SymbolicConstant specifying the direction of feature creation. Possible values are SIDE1 and SIDE2.
-sketchUpEdge
-An Edge object or a Datum axis object specifying the vertical (Y) direction of the sketch.
-sketch
-A ConstrainedSketch object specifying the planar sketch to be revolved.
-'''
-
-'''
-# Solid loft
-
-
-Path
-
-mdb.models[name].parts[name].SolidLoft
-
-
-Required arguments
-loftsections
-A sequence of sequences of edges specifying the cross-sections to be lofted. Each outer sequence specifies a section through which Abaqus will pass the loft feature. Each outer sequence can be defined as a sequence of edges or as an EdgeArray. The edges specifying a section must form a simple closed profile and must not contain multiple loops.
-
-
-import part
-mdb.models[name].parts[name].allInternalSets[name].edges[i]
-mdb.models[name].parts[name].allInternalSurfaces[name].edges[i]
-mdb.models[name].parts[name].allSets[name].edges[i]
-mdb.models[name].parts[name].allSurfaces[name].edges[i]
-mdb.models[name].parts[name].edges[i]
-mdb.models[name].parts[name].sets[name].edges[i]
-mdb.models[name].parts[name].surfaces[name].edges[i]
-
-
-# rough_data = np.genfromtxt(surfFile, delimiter=',', skip_header=1)
-# samplingPt = len(rough_data)
-
-# topSurfPt = 
-
-# s.Spline(points = topSurfPt)
-# if isTopHalf == 0:
-#   s.Line(point1 = (-0.5*boxsize, -0.5*boxsize), point2 = (0.5*boxsize, -0.5*boxsize))
-#   s.Line(point1 = (0.5*boxsize,  -0.5*boxsize), point2 = (0.5*boxsize, topSurfPt[(-1,1)]))
-#   s.Line(point1 = (-0.5*boxsize, -0.5*boxsize), point2 = (-0.5*boxsize, topSurfPt[(0,1)]))
-# if isTopHalf == 1:
-#   s.Line(point1 = (-0.5*boxsize, 0*boxsize), point2 = (0.5*boxsize, 0*boxsize))
-#   s.Line(point1 = (0.5*boxsize,  0*boxsize), point2 = (0.5*boxsize, topSurfPt[(-1,1)]))
-#   s.Line(point1 = (-0.5*boxsize, 0*boxsize), point2 = (-0.5*boxsize, topSurfPt[(0,1)]))
-
-
-# ## Draw outer square
-# s.rectangle(point1=(-2.0*boxsize, -2.0*boxsize), point2=(2.0*boxsize, 2.0*boxsize))
-
-
-# ## Extrude part
-# pbox = myModel.Part(name='box', dimensionality=THREE_D, type=DEFORMABLE_BODY)
-# try:
-#   pbox.BaseSolidExtrude(sketch=s, depth=boxsize)
-#   boxerr = 0
-#   print('Create box successfully')
-# except:
-#   boxerr = 1
-#   print('Recreate box')
-# del myModel.sketches['__profile__']
-  
 # ------------------------------------------------------------------------------------------------------------
 
-# Create a plate to be cut
-## Sketch part
-r = myModel.ConstrainedSketch(name = '__profile__', sheetSize = boxsize*2)
-r.rectangle(point1=(-0.7*boxsize, -0.7*boxsize), point2=(0.7*boxsize, 0.7*boxsize))
+# Create surface box
+## Draw first 3D wire as base on first plane
+s = myModel.ConstrainedSketch(name = '__profile__', sheetSize = 200.0)
+g, v, d, c = s.geometry, s.vertices, s.dimensions, s.constraints
+s.sketchOptions.setValues(decimalPlaces=6)
 
-## Extrude part
-p = myModel.Part(dimensionality=THREE_D, name='plate', type=DEFORMABLE_BODY)
-p.BaseSolidExtrude(depth=boxsize, sketch=r)
+topSurfPt = pt[0]
+leftEnd = topSurfPt[0][1]
+rightEnd = topSurfPt[-1][1]
+
+s.Spline(points = topSurfPt)
+s.Line(point1 = (-0.5*boxsize, -0.5*boxsize), point2 = (0.5*boxsize, -0.5*boxsize))
+s.Line(point1 = (0.5*boxsize,  -0.5*boxsize), point2 = (0.5*boxsize, rightEnd))
+s.Line(point1 = (-0.5*boxsize, -0.5*boxsize), point2 = (-0.5*boxsize, leftEnd))
+
+myPart = myModel.Part(dimensionality=THREE_D, name='Part-1', type=DEFORMABLE_BODY)
+myPart.BaseWire(sketch=s)
 
 del myModel.sketches['__profile__']
 
 # ------------------------------------------------------------------------------------------------------------
 
+# Loop to create sketch on plane
+
+for i in range(1, boxsize+1):
+
+	## Create datum plane
+	myPart.DatumPlaneByPrincipalPlane(offset=i, principalPlane=XYPLANE)
+
+	## Draw sketch
+	s = myModel.ConstrainedSketch(name='__profile__', sheetSize=200.0, transform=
+		myPart.MakeSketchTransform(sketchPlane=myPart.datums[2*i], origin=(0,0,i)))
+	s.sketchOptions.setValues(decimalPlaces=6)
+
+	topSurfPt = pt[i]
+
+	a = topSurfPt[0]
+	leftEnd = a[1]
+	b = topSurfPt[-1]
+	rightEnd = b[1]
+
+	s.Spline(points = topSurfPt)
+	s.Line(point1 = (-0.5*boxsize, -0.5*boxsize), point2 = (0.5*boxsize, -0.5*boxsize))
+	s.Line(point1 = (0.5*boxsize,  -0.5*boxsize), point2 = (0.5*boxsize, rightEnd))
+	s.Line(point1 = (-0.5*boxsize, -0.5*boxsize), point2 = (-0.5*boxsize, leftEnd))
+
+	myPart.Wire(sketch=s, sketchPlane=myPart.datums[2*i])
+
+	del myModel.sketches['__profile__']
+
+# ------------------------------------------------------------------------------------------------------------
+
+# Solid loft
+
+loftPlane_list = []
+for i in range(0, boxsize+1):
+    edge_i = (myPart.edges.getByBoundingBox(xMin=-boxsize, xMax=boxsize, yMin=-boxsize, yMax=boxsize, zMin=-0.5+i, zMax=0.5+i))
+    loftPlane_list.append(edge_i)
+
+try:
+    (mdb.models['Model-1'].parts['Part-1'].SolidLoft(endCondition=NONE, loftsections=loftPlane_list, startCondition=NONE))
+except:
+    print('Solid loft error')
+    sys.exit()
+
+# ------------------------------------------------------------------------------------------------------------
+
+## Create YZ-datum plane for partition
+for i in range(1, boxsize):
+    myPart.DatumPlaneByPrincipalPlane(offset=i-boxsize/2, principalPlane=YZPLANE)
+
+## Create XZ-datum plane for partition
+myPart.DatumPlaneByPrincipalPlane(offset=partitionH, principalPlane=XZPLANE)
+
+## count no. of datum plane
+### xy-datum: 2,4,6,...,boxsize*2 = 20 planes (not included most left)
+### yz-datum: boxsize*2+2+1, boxsize*2+2+2, ..., boxsize*2+2+(boxsize-1) = 19 planes (not inluded both end sides)
+### xz-datum: boxsize*2+2+(boxsize-1)+1 = 1 plane
+
+start_yzDatum = boxsize*2+2
+start_xzDatum = boxsize*2+2+(boxsize-1)+1
+
+# ------------------------------------------------------------------------------------------------------------
+
+# Partitioning
+## XZ-plane partition
+myPart.PartitionCellByDatumPlane(cells=myPart.cells, datumPlane=myPart.datums[start_xzDatum])
+
+## Create set for upperBox
+upperBox = myPart.Set(name='upperBox', cells=myPart.cells.getByBoundingBox(
+    xMin=-boxsize, xMax=boxsize, yMin=0.2*boxsize, yMax=boxsize, zMin=-boxsize, zMax=boxsize))
+
+## XY-plane partition
+for i in range(1, boxsize):
+    myPart.PartitionCellByDatumPlane(cells=upperBox.cells, datumPlane=myPart.datums[2*i])
+
+## YZ-plane partition
+for i in range(1, boxsize):
+    myPart.PartitionCellByDatumPlane(cells=upperBox.cells, datumPlane=myPart.datums[start_yzDatum+i])
+
+# ------------------------------------------------------------------------------------------------------------
+
 # Assembly
-## Declare assembly
-a = mdb.models['Model-1'].rootAssembly
-
-## Instance box part
-a.Instance(name='box-0', part=pbox, dependent=ON)
-## Instance plate part
-a.Instance(name='Plate_beforeCut', part=p, dependent=ON)
-
-## Cut part by box
-a.InstanceFromBooleanCut(name='Cut_Part', 
-    instanceToBeCut=mdb.models['Model-1'].rootAssembly.instances['Plate_beforeCut'], 
-    cuttingInstances=(a.instances['box-0'], ), 
-    originalInstances=DELETE)
-a.makeIndependent(instances=(a.instances['Cut_Part-1'], ))
-
-'''
+myAssembly = mdb.models['Model-1'].rootAssembly
+myAssembly.Instance(name='Cut_Part-1', part=myPart, dependent=ON)
+myAssembly.makeIndependent(instances=(myAssembly.instances['Cut_Part-1'], ))
