4c8765bddff77e6107fda3fb18439599ab84739d
Pakaporn Tangnuntachai <pakaporn.tangnuntachai@gmail.com>
2020-02-15 08:02:00 +0100

    add description getLocalElem_submodel

--- hole_expansion/postprocessing/getLocalElems.py
+++ hole_expansion/postprocessing/getLocalElems.py
@@ -3,24 +3,28 @@ from abaqusConstants import *
 from odbAccess import *
 from odbAccess import *
 from odbSection import *
+import numpy as np
 
 path = ''
-caeName = 'HET_submodel_1_re' # without .cae
+caeName = 'HET_submodel3D_1' # without .cae
 if caeName[-4:] == '.cae': caeName = caeName[:-4]
-# boxsize = 300
-# localPoint:   149-1, rd96: (-39.140285, 45.293041)
-#               149-1, rd48: ( 39.065548, 44.745098)
-localPoint = (4997.49,32,182.451)
-localRadius = 5
-shiftRatio = 0.1
+
+localPoint = (4997.49, 32, 182.451)
+localRadius = 2.5
+shiftRatio = 0.1    # 1 = whole sphere in submodel box
+					# -1 = whole sphere out of submodel box
+					# 0.8 = comparable to localRatioY = 0.9 of getByBoundingBox
+
 # # # Define name
 instanceName = 'subPart'
 stepName = 'move'
+
 # # Start extraction
 x, y, z = localPoint
 shiftVect = [x, z]/np.linalg.norm([x, z])
 x_shift, z_shift = np.array([x, z]) + shiftVect*localRadius*shiftRatio
 localPoint = (x_shift, y, z_shift)
+
 mdb = openMdb(path + caeName + '.cae')
 caeAsm = mdb.models['Model-1'].rootAssembly.instances['subPart']
 elemLocal = caeAsm.elements.getByBoundingSphere(center=localPoint, radius=localRadius)
